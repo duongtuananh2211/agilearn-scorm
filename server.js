993753d9f405a1fe88fbbdad2af3ee99027ba06d
anhdt <anhdt@demetio.com>
2025-06-07 15:05:53 +0700

    ADD: custom server

--- server.js
+++ server.js
@@ -21,8 +21,36 @@ app.prepare().then(() => {
           return handle(req, res, parsedUrl);
         }
 
-        // If file exists, serve it directly
-        res.writeHead(200, { "Content-Type": "application/octet-stream" });
+        if (parsedUrl.pathname.endsWith(".html")) {
+          res.writeHead(200, { "Content-Type": "text/html" });
+        } else if (parsedUrl.pathname.endsWith(".js")) {
+          res.writeHead(200, { "Content-Type": "application/javascript" });
+        } else if (parsedUrl.pathname.endsWith(".css")) {
+          res.writeHead(200, { "Content-Type": "text/css" });
+        } else if (parsedUrl.pathname.endsWith(".json")) {
+          res.writeHead(200, { "Content-Type": "application/json" });
+        } else if (parsedUrl.pathname.endsWith(".png")) {
+          res.writeHead(200, { "Content-Type": "image/png" });
+        } else if (
+          parsedUrl.pathname.endsWith(".jpg") ||
+          parsedUrl.pathname.endsWith(".jpeg")
+        ) {
+          res.writeHead(200, { "Content-Type": "image/jpeg" });
+        } else if (parsedUrl.pathname.endsWith(".svg")) {
+          res.writeHead(200, { "Content-Type": "image/svg+xml" });
+        } else if (
+          parsedUrl.pathname.endsWith(".woff") ||
+          parsedUrl.pathname.endsWith(".woff2")
+        ) {
+          res.writeHead(200, { "Content-Type": "font/woff" });
+        } else if (parsedUrl.pathname.endsWith(".ttf")) {
+          res.writeHead(200, { "Content-Type": "font/ttf" });
+        } else if (parsedUrl.pathname.endsWith(".ico")) {
+          res.writeHead(200, { "Content-Type": "image/x-icon" });
+        } else {
+          res.writeHead(200, { "Content-Type": "application/octet-stream" });
+        }
+
         const readStream = fs.createReadStream(filePath);
         readStream.pipe(res);
       });
