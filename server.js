cb5bac6a2a69409756c26079a6a35076eb462663
anhdt <anhdt@demetio.com>
2025-06-22 12:50:22 +0700

    MOD: handle cors

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.prepare().then(() => {
         return;
       }
 
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      res.writeHead(200, { "Content-Type": "text/html" });
+
       res.end(`<!DOCTYPE html>
         <html>
           <head>
